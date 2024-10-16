20801476f3dbc3a05b06b7c6757659443aac035e
alktcys <alktcys@gmail.com>
2024-10-16 07:38:11 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 내가 만든 앱")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "자 이제 시작이야!!!"
 )
